7a28b44525de1a3f1461dc3f426af5f41326c7ab
stephanesordet <stsordet@gmail.com>
2022-05-26 16:32:56 +0200

    Techno changes

---
+++ Code_FrontEnd/composables/fetch.js
@@ -0,0 +1,16 @@
+import { ref } from "vue";
+
+export function useFetch(url){
+
+    const data = ref(null)
+
+    async function loadJson(){
+        const res = await fetch(url)
+        const json = await res.json()
+        data.value = json
+    }
+
+    loadJson()
+
+    return { data }
+}
---
+++ Code_FrontEnd/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+import { ref, watchEffect } from 'vue';
+import { useFetch } from '../composables/fetch'
+
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
