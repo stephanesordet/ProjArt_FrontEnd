54435f87e25192d1fd6dfac324def15fe7cfccf0
Lucas1564 <lucascuennet@gmail.com>
2022-05-30 13:37:58 +0200

    Resolve problem with routing2

--- Code_FrontEnd/src/components/TheNavbar.vue
+++ Code_FrontEnd/src/components/TheNavbar.vue
@@ -43,7 +43,7 @@ import { ref } from 'vue';
 <template>
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="">
+    <a class="navbar-item" href="#accueil">
       <span>LOGO</span>
       <!-- <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"> -->
     </a>
