b725e50aebc81131828e1be80c9edabb0984e577
Lara Hörler <lara.horler@heig-vd.ch>
2022-05-31 14:47:27 +0200

    CardNotif done

--- Code_FrontEnd/src/Notifications.vue
+++ Code_FrontEnd/src/Notifications.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import TheCardNotif from "./components/TheCardNotif.vue";
+import BaseBoutonRetour from "./components/BaseBoutonRetour.vue";
+import { useFetch } from "../composables/fetch";
+
+/* const { data: notif } = useFetch(
+  ""
+); */
+
+</script>
+
 
 <template>
+
   <h1>notif</h1>
+  <base-bouton-retour></base-bouton-retour>
+<!--   <the-card-notif v-for="notif in notifs" :key="notif.id" :user="notif.user" :object="notif.object" :message="notif.message" :envoiHeureDate="notif.envoiHeureDate"></the-card-notif>
+ --> 
+<the-card-notif> </the-card-notif>  
 </template>
 
-<script scoped></script>
+<style scoped> 
+</style>
---
+++ Code_FrontEnd/src/components/TheCardNotif.vue
@@ -0,0 +1,69 @@
+<script setup>
+defineProps({
+  user: {
+    type: String,
+  },
+  object: {
+    type: String,
+  },
+  message: {
+    type: String,
+  },
+  envoiHeureDate: {
+    type: String,
+  },
+});
+</script> 
+
+<template>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- <div class="columns">
+  <div class="is-half is-offset-one-quarter has-text-left column">
+<div class="box">
+        <p>
+          <strong> {{ user}} - {{ object}} </strong>  <small>{{envoiHeureDate}}</small>
+          <br>
+          {{message}}
+        </p>
+        </div>
+    </div>
+    </div> -->
+
+  
+<div class="columns ">
+  <div class="is-half is-offset-one-quarter has-text-left column">
+
+<div class="box is-child notification is-primary has-text-left-mobile has-icons-right">
+             
+          <p>
+               
+
+          <strong>AGE - Soirée CultureG</strong>  <small>13:05</small>     
+  <span class="icon is-right">
+
+            <i class="fa fa-solid fa-circle"></i>
+  </span>
+         
+          <br>
+          
+          Venez nous rejoindre pour une soirée culturel général !
+
+
+        </p>
+     
+
+        </div>
+
+
+
+    </div>
+    </div>
+
+
+  
+</template>
+
+
+<style scoped>
+</style>
