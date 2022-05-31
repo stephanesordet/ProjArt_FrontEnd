f2eb66e2df1cb435d4d9b6ba474d0e4a9d9bc399
Alexia Leger <alexia.leger@heig-vd.ch>
2022-05-31 11:11:58 +0200

    Correction TheNavBar

--- Code_FrontEnd/src/components/TheNavbar.vue
+++ Code_FrontEnd/src/components/TheNavbar.vue
@@ -45,15 +45,6 @@ const props = defineProps({
         <img id="logoNavbar" src="../assets/logoProjArt.png" />
       </a>
 
-      <a class="navbar-item" href="#evenements">
-        Evenements
-      </a>
-      <a class="navbar-item" href="#agendaClasse">
-        Agenda des classes
-      </a>
-      <a class="navbar-item" href="#detailMatiere">
-        Détails de la matiere
-      </a>
       <a
         role="button"
         class="navbar-burger"
@@ -73,7 +64,7 @@ const props = defineProps({
 
         <a class="navbar-item" href="#evenements"> Evenements </a>
         <a class="navbar-item" href="#agendaClasse"> Agenda des classes </a>
-        <a class="navbar-item" href="#detailCours"> Détails du cours </a>
+        <a class="navbar-item" href="#detailMatiere"> Détails de la matière </a>
       </div>
 
       <div class="navbar-end">
