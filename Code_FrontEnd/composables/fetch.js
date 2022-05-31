f90d986a114b417c75474e96df2b0b7d7644bf64
Stéphane Sordet <stsordet@gmail.com>
2022-05-31 11:13:20 +0200

    Accueil 2.0

--- Code_FrontEnd/composables/fetch.js
+++ Code_FrontEnd/composables/fetch.js
@@ -1,16 +1,15 @@
 import { ref } from "vue";
 
-export function useFetch(url){
+export function useFetch(url) {
+  const data = ref(null);
 
-    const data = ref(null)
+  async function loadJson() {
+    const res = await fetch(url);
+    const json = await res.json();
+    data.value = json;
+  }
 
-    async function loadJson(){
-        const res = await fetch(url)
-        const json = await res.json()
-        data.value = json
-    }
+  loadJson();
 
-    loadJson()
-
-    return { data }
+  return { data };
 }
--- Code_FrontEnd/src/main.js
+++ Code_FrontEnd/src/main.js
@@ -1,13 +1,13 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/saga-blue/theme.css'       //theme
-import 'primevue/resources/primevue.min.css'                 //core css
-import 'primeicons/primeicons.css'
-import Button from 'primevue/button'                          //icons
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css";
+import Button from "primevue/button"; //icons
 
 const app = createApp(App);
-app.use(PrimeVue)
+app.use(PrimeVue);
 
-app.component('p-button', Button)
-app.mount('#app')
+app.component("p-button", Button);
+app.mount("#app");
