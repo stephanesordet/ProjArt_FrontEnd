bae0aea596e649b334fb392db2f49a51ce6cd5af
Lara Hörler <lara.horler@heig-vd.ch>
2022-06-01 08:57:45 +0200

    notif modif

--- Code_FrontEnd/src/Notifications.vue
+++ Code_FrontEnd/src/Notifications.vue
@@ -1,6 +1,7 @@
 <script setup>
+import TheCardWrapper from "./components/TheCardWrapper.vue";
 import CardNotif from "./components/CardNotif.vue";
-import BaseBoutonRetour from "./components/TheReturnButton.vue";
+import TheReturnButton from "./components/TheReturnButton.vue";
 import { useFetch } from "../composables/fetch";
 
 /* const { data: notif } = useFetch(
@@ -13,11 +14,16 @@ import { useFetch } from "../composables/fetch";
 <template>
 
   <h1>notif</h1>
-
+<the-return-button> </the-return-button>
   <!--   <the-card-notif v-for="notif in notifs" :key="notif.id" :user="notif.user" :object="notif.object" :message="notif.message" :envoiHeureDate="notif.envoiHeureDate"></the-card-notif>
- -->
-  <card-notif></card-notif>
+ --> 
+<the-card-wrapper>
+      <card-notif :user="'AGE'" :object="'Soirée CultureG'" :envoiHeureDate="'13:05'" :message="'Venez nombreux !'">
+      </card-notif>
+    </the-card-wrapper>
+
+
 </template>
 
-<style scoped>
+<style scoped> 
 </style>
