12e6cc7436b93c3deb6cab1fe12c09504bc7c457
Lucas1564 <lucascuennet@gmail.com>
2022-06-01 15:08:55 +0200

    Add base components form

---
+++ Code_FrontEnd/src/components/BaseForm.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <form class="main my-4 mx-4">
+    <div class="containerInputs">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+form {
+  background-color: #ebeeef;
+  padding: 20px;
+}
+
+.containerInputs {
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+</style>
---
+++ Code_FrontEnd/src/components/BaseInput.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="field">
+    <label class="label">
+      <slot name="label"></slot>
+    </label>
+
+    <div class="control">
+      <slot name="input"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div :slotted(*) {
+  width: 300px;
+}
+</style>
