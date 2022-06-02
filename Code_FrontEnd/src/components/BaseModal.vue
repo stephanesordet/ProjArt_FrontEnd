691a617a1344538bc01aa873d0d341ab85174773
Alexia Leger <alexia.leger@heig-vd.ch>
2022-06-02 09:24:12 +0200

    Modifié bouton modal

--- Code_FrontEnd/src/components/BaseModal.vue
+++ Code_FrontEnd/src/components/BaseModal.vue
@@ -8,9 +8,9 @@
         <div class="modal-content box has-background-light is-rounded">
             <p><strong>Etes vous sûr(e) de vouloir annoncer votre retard ?</strong></p>
             <p>Votre classe ainsi que votre professeur sera notifié de votre retard</p>
-            <button class="button is-danger" @click="$emit('confirm')">Confirmer</button>
+            <button class="button is-danger is-rounded is-pulled-right" @click="$emit('confirm')">Confirmer</button>
         </div>
-        <button class="button modal-close is-large is-danger is-right" aria-label="close" @click="$emit('cancel')"></button>
+        <button class="button modal-close is-large is-danger is-right is-rounded" aria-label="close" @click="$emit('cancel')"></button>
     </div>
 </template>
 
