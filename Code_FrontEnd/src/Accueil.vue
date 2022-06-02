db1b00007cf9d6edaf3b50882b18915193bb5fab
Lucas1564 <lucascuennet@gmail.com>
2022-06-02 10:18:07 +0200

    Add ajouts forms

--- Code_FrontEnd/src/Accueil.vue
+++ Code_FrontEnd/src/Accueil.vue
@@ -8,8 +8,8 @@ const { data: horaires } = useFetch(
   "https://chabloz.eu/files/horaires/all.json"
 );
 
-let selectedClasse = ref('IM48');
-let selectedMatiere = ref('Tous les cours')
+let selectedClasse = ref("IM48");
+let selectedMatiere = ref("Tous les cours");
 let Matieres = ref([]);
 
 const Classes = computed(() => {
@@ -32,7 +32,10 @@ Matieres = computed(() => {
     return [];
   } else {
     horaires.value.forEach((element) => {
-      if (!tabMatieres.includes(element.label) && element.class == selectedClasse.value) {
+      if (
+        !tabMatieres.includes(element.label) &&
+        element.class == selectedClasse.value
+      ) {
         tabMatieres.push(element.label);
       }
     });
@@ -41,49 +44,87 @@ Matieres = computed(() => {
 });
 
 function afficheForm() {
-  console.log(4)
+  console.log(4);
 }
-
 </script>
 
 <template>
   <div class="main mx-4 my-1">
     <div>
       <div class="buttons is-mobile columns is-centered mx-1 my-1">
+<<<<<<< Updated upstream
         <button v-for="classe in Classes" :key="classe"
           @click="selectedClasse = classe, selectedMatiere = 'Tous les cours'"
           class="column button has-background-light has-text-black is-medium is-one-fifth-mobile is-danger">
+=======
+        <button
+          v-for="classe in Classes"
+          :key="classe"
+          @click="
+            (selectedClasse = classe), (selectedMatiere = 'Tous les cours')
+          "
+          class="column button has-background-info has-text-white is-medium is-one-fifth-mobile"
+        >
+>>>>>>> Stashed changes
           {{ classe }}
         </button>
       </div>
     </div>
     <div class="select is-danger">
       <select>
-        <option @click="selectedMatiere = 'Tous les cours'">Tous les cours</option>
-        <option v-for=" matiere in Matieres" :key="matiere" @click="selectedMatiere = matiere">
+        <option @click="selectedMatiere = 'Tous les cours'">
+          Tous les cours
+        </option>
+        <option
+          v-for="matiere in Matieres"
+          :key="matiere"
+          @click="selectedMatiere = matiere"
+        >
           {{ matiere }}
         </option>
       </select>
     </div>
     <div class="columns is-centered tile is-ancestor">
       <div class="column is-three-quarters">
-        <div v-if="selectedMatiere != 'Tous les cours'" class="tile is-parent is-vertical">
-          <card-cours v-for="horaire in horaires" :key="horaire.start"
-            v-show="selectedClasse == horaire.class && selectedMatiere == horaire.label" :debut="horaire.start"
-            :fin="horaire.start" :cours="horaire.label" :salle="horaire.room">
+        <div
+          v-if="selectedMatiere != 'Tous les cours'"
+          class="tile is-parent is-vertical"
+        >
+          <card-cours
+            v-for="horaire in horaires"
+            :key="horaire.start"
+            v-show="
+              selectedClasse == horaire.class &&
+              selectedMatiere == horaire.label
+            "
+            :debut="horaire.start"
+            :fin="horaire.start"
+            :cours="horaire.label"
+            :salle="horaire.room"
+          >
           </card-cours>
         </div>
 
         <div v-else class="tile is-parent is-vertical">
-          <card-cours v-for="horaire in horaires" :key="horaire.id" v-show="selectedClasse == horaire.class"
-            :debut="horaire.start" :fin="horaire.start" :cours="horaire.label" :salle="horaire.room">
+          <card-cours
+            v-for="horaire in horaires"
+            :key="horaire.id"
+            v-show="selectedClasse == horaire.class"
+            :debut="horaire.start"
+            :fin="horaire.start"
+            :cours="horaire.label"
+            :salle="horaire.room"
+          >
           </card-cours>
-
         </div>
       </div>
     </div>
     <div>
-      <button class="button is-pulled-right" id="fixedbutton" @click="afficheForm()">
+      <button
+        class="button is-pulled-right"
+        id="fixedbutton"
+        @click="afficheForm()"
+      >
         <span class="icon is-large has-text-danger">
           <i class="fa fa-4x fa-plus-square"></i>
         </span>
--- Code_FrontEnd/src/FormAjoutCours.vue
+++ Code_FrontEnd/src/FormAjoutCours.vue
@@ -59,6 +59,7 @@ function addCours() {
 </script>
 
 <template>
+  <!-- AJOUT COURS  -->
   <BaseForm @submit.prevent="addCours()">
     <!--     <link
       href="~bulma-calendar/dist/css/bulma-calendar.min.css"
@@ -147,7 +148,272 @@ function addCours() {
     <BaseInputSubmit>
       <input
         type="submit"
-        class="button is-primary is-rounded"
+        class="button is-danger is-rounded"
+        value="Ajouter le cours"
+      />
+    </BaseInputSubmit>
+  </BaseForm>
+
+  <!-- AJOUT EVENT  -->
+  <BaseForm @submit.prevent="addEvent()">
+    <!--     <link
+      href="~bulma-calendar/dist/css/bulma-calendar.min.css"
+      rel="stylesheet"
+    /> -->
+    <h1 class="title is-1">Nouveau évènement</h1>
+
+    <BaseInput>
+      <template v-slot:label>Date</template>
+      <template v-slot:input>
+        <input
+          v-model="date"
+          class="input"
+          type="date"
+          placeholder="Entrez une date"
+        />
+        <!--           <input
+            type="date"
+            data-display-mode="inline"
+            data-is-range="true"
+            data-close-on-select="false"
+          /> -->
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Titre</template>
+      <template v-slot:input>
+        <input
+          v-model="eventTitle"
+          class="input"
+          type="text"
+          placeholder="Entrez le nom de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Description</template>
+      <template v-slot:input>
+        <input
+          v-model="eventDescription"
+          class="input"
+          type="text"
+          placeholder="Entrez la description de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Heure de début</template>
+      <template v-slot:input>
+        <input
+          v-model="heureDebut"
+          class="input"
+          type="time"
+          placeholder="Entrez une heure de début"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Heure de fin</template>
+      <template v-slot:input>
+        <input
+          v-model="heureFin"
+          class="input"
+          type="time"
+          placeholder="Entrez une heure de fin"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInputSubmit>
+      <input
+        type="submit"
+        class="button is-danger is-rounded"
+        value="Ajouter le cours"
+      />
+    </BaseInputSubmit>
+  </BaseForm>
+
+  <!-- AJOUT REMARQUE COURS -->
+  <BaseForm @submit.prevent="addRemarqueCours()">
+    <!--     <link
+      href="~bulma-calendar/dist/css/bulma-calendar.min.css"
+      rel="stylesheet"
+    /> -->
+    <h1 class="title is-1">Nouvelle remarque pour : nomCours</h1>
+
+    <div class="field" style="width: 300px">
+      <label class="label" for="Remarque">Remarque</label>
+
+      <div class="control">
+        <input type="radio" id="html" name="fav_language" value="HTML" />
+        <label for="html"> Publique</label><br />
+        <input type="radio" id="css" name="fav_language" value="CSS" />
+        <label for="css"> Privée</label><br />
+      </div>
+    </div>
+
+    <BaseInput>
+      <template v-slot:label>Date</template>
+      <template v-slot:input>
+        <input
+          v-model="date"
+          class="input"
+          type="date"
+          placeholder="Entrez une date"
+        />
+        <!--           <input
+            type="date"
+            data-display-mode="inline"
+            data-is-range="true"
+            data-close-on-select="false"
+          /> -->
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Titre</template>
+      <template v-slot:input>
+        <input
+          v-model="eventTitle"
+          class="input"
+          type="text"
+          placeholder="Entrez le nom de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Description</template>
+      <template v-slot:input>
+        <input
+          v-model="eventDescription"
+          class="input"
+          type="text"
+          placeholder="Entrez la description de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInputSubmit>
+      <input
+        type="submit"
+        class="button is-danger is-rounded"
+        value="Ajouter le cours"
+      />
+    </BaseInputSubmit>
+  </BaseForm>
+
+  <!-- AJOUT REMARQUE PRIVÉE  -->
+  <BaseForm @submit.prevent="addRemarquePrivate()">
+    <!--     <link
+      href="~bulma-calendar/dist/css/bulma-calendar.min.css"
+      rel="stylesheet"
+    /> -->
+    <h1 class="title is-1">Nouvelle remarque privée</h1>
+
+    <BaseInput>
+      <template v-slot:label>Date</template>
+      <template v-slot:input>
+        <input
+          v-model="date"
+          class="input"
+          type="date"
+          placeholder="Entrez une date"
+        />
+        <!--           <input
+            type="date"
+            data-display-mode="inline"
+            data-is-range="true"
+            data-close-on-select="false"
+          /> -->
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Matière</template>
+      <template v-slot:input>
+        <div class="select">
+          <select v-model="matiere">
+            <option>Matière 1</option>
+            <option>Matière 2</option>
+          </select>
+        </div>
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Titre</template>
+      <template v-slot:input>
+        <input
+          v-model="eventTitle"
+          class="input"
+          type="text"
+          placeholder="Entrez le nom de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Description</template>
+      <template v-slot:input>
+        <input
+          v-model="eventDescription"
+          class="input"
+          type="text"
+          placeholder="Entrez la description de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInputSubmit>
+      <input
+        type="submit"
+        class="button is-danger is-rounded"
+        value="Ajouter le cours"
+      />
+    </BaseInputSubmit>
+  </BaseForm>
+
+  <!-- AJOUT NOTIFICATION  -->
+  <BaseForm @submit.prevent="addNotification()">
+    <!--     <link
+      href="~bulma-calendar/dist/css/bulma-calendar.min.css"
+      rel="stylesheet"
+    /> -->
+    <h1 class="title is-1">Nouvelle notification</h1>
+
+    <BaseInput>
+      <template v-slot:label>Titre</template>
+      <template v-slot:input>
+        <input
+          v-model="eventTitle"
+          class="input"
+          type="text"
+          placeholder="Entrez le nom de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInput>
+      <template v-slot:label>Description</template>
+      <template v-slot:input>
+        <input
+          v-model="eventDescription"
+          class="input"
+          type="text"
+          placeholder="Entrez la description de l'évènement"
+        />
+      </template>
+    </BaseInput>
+
+    <BaseInputSubmit>
+      <input
+        type="submit"
+        class="button is-danger is-rounded"
         value="Ajouter le cours"
       />
     </BaseInputSubmit>
