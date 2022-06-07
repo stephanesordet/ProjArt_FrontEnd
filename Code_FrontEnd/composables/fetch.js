263b3bcd4c6b28e84ce2e5160abbf9ec6547cc49
Jules Sandoz <jules.sandoz@heig-vd.ch>
2022-06-07 15:31:27 +0200

    worked on login

--- Code_FrontEnd/composables/fetch.js
+++ Code_FrontEnd/composables/fetch.js
@@ -8,26 +8,34 @@ export function useFetch(url) {
     const res = await fetch(url);
     const json = await res.json();
     data.value = json;
+    return {data};
   }
 
   loadJson();
-
   return { data };
 }
 
-export function useFetchLogin(user, mdp) {
-  
-
-  async function loadHtml() {
-    const res = await fetch('http://127.0.0.1:8000/api/login/' + mdp + '/' + user);
+export async function useFetchLogin(user, mdp) {
+  console.log(mdp);
+  async function loadHtml(email, password) {
+    const u = /* encodeURIComponent */(email);
+    const p = /* encodeURIComponent */(password);
+    const res = await fetch('http://127.0.0.1:8000/api/login/' + p + '/' + u);
     const html = await res.text();
     if(html == 'user found and connected'){
+      alert("Connexion réussie");
+      sessionStorage.setItem("user", email);
+      window.location.href = "#accueil";
+      window.location.reload();
       console.log('hello')
       return true;
+    } else {
+      html == 'User not found'
+      alert("Connexion échouée");
+      return false;
     }
   }
 
-  loadHtml();
+ loadHtml(user, mdp);
 
 }
-
--- Code_FrontEnd/src/Accueil.vue
+++ Code_FrontEnd/src/Accueil.vue
@@ -14,7 +14,7 @@ const { data: matieres } = useFetch("http://127.0.0.1:8000/api/matiere");
 
 const { data: classes } = useFetch("http://127.0.0.1:8000/api/classes");
 
-const { data: coursClasse } = useFetch('http://127.0.0.1:8000/api/cours/classe/IM49-2');
+const { data: coursClasse } = useFetch("http://127.0.0.1:8000/api/cours/classe/IM49-2");
 
 const CoursClasse = computed(() => {
   const tabCours = [];
@@ -22,7 +22,7 @@ const CoursClasse = computed(() => {
     return [];
   } else {
     coursClasse.value.forEach((element) => {
-      tabCours.push(element)
+      tabCours.push(element);
     });
   }
   return tabCours;
@@ -45,47 +45,65 @@ const Classes = computed(() => {
 function afficheForm() {
   alert("aaa");
 }
-
 </script>
 
 <template>
   <div class="main mx-4 my-1">
     <div>
       <div class="buttons is-mobile columns is-centered mx-1 my-1">
-        <button v-for="classe in Classes" :key="classe" @click="
-          coursClasse.value = useFetch('http://127.0.0.1:8000/api/cours/classe/' + classe)
-        " class="
-            column
-            button
-            has-background-light has-text-black
-            is-medium is-one-fifth-mobile is-danger
-          ">
+        <button
+          v-for="classe in Classes"
+          :key="classe"
+          @click="
+            coursClasse.value = useFetch(
+              'http://127.0.0.1:8000/api/cours/classe/' + classe
+            ).value
+          "
+          class="column button has-background-light has-text-black is-medium is-one-fifth-mobile is-danger"
+        >
           {{ classe }}
         </button>
       </div>
     </div>
     <div class="select is-danger">
       <select>
-        <option @click="selectedMatiere = 'Tous les cours'">
-          Tous les cours
-        </option>
-        <option v-for="cours in CoursClasse" :key="cours.id" @click="selectedMatiere = cours.matiere_id">
+        <option @click="selectedMatiere = 'Tous les cours'">Tous les cours</option>
+        <option
+          v-for="cours in CoursClasse"
+          :key="cours.id"
+          @click="selectedMatiere = cours.matiere_id"
+        >
           {{ cours.matiere_id }}
         </option>
       </select>
     </div>
     <div class="columns is-centered tile is-ancestor">
       <div class="column is-three-quarters">
-        <div v-if="selectedMatiere != 'Tous les cours'" class="tile is-parent is-vertical">
-          <card-cours v-for="cours in coursClasse" :key="cours.id" v-show="selectedMatiere == cours.matiere_id"
-            :debut="cours.Debut" :fin="cours.Fin" :cours="cours.matiere_id" :salle="cours.salle_id">
+        <div
+          v-if="selectedMatiere != 'Tous les cours'"
+          class="tile is-parent is-vertical"
+        >
+          <card-cours
+            v-for="cours in coursClasse"
+            :key="cours.id"
+            v-show="selectedMatiere == cours.matiere_id"
+            :debut="cours.Debut"
+            :fin="cours.Fin"
+            :cours="cours.matiere_id"
+            :salle="cours.salle_id"
+          >
           </card-cours>
         </div>
 
         <div v-else class="tile is-parent is-vertical">
-          <card-cours v-for="cours in coursClasse" :key="cours.id" :debut="cours.Debut" :fin="cours.Fin"
-            :cours="cours.matiere_id" :salle="cours.salle_id">
-
+          <card-cours
+            v-for="cours in coursClasse"
+            :key="cours.id"
+            :debut="cours.Debut"
+            :fin="cours.Fin"
+            :cours="cours.matiere_id"
+            :salle="cours.salle_id"
+          >
           </card-cours>
         </div>
       </div>
@@ -104,10 +122,7 @@ function afficheForm() {
     </div>
   </div>
   <!-- MODAL FORM  -->
-  <BaseModalForm
-    :class="{ 'is-active': showModalForm }"
-    @close="showModalForm = false"
-  >
+  <BaseModalForm :class="{ 'is-active': showModalForm }" @close="showModalForm = false">
     <!-- AJOUT COURS  -->
     <BaseFormModal @submit.prevent="addCours()">
       <!--     <link
@@ -119,12 +134,7 @@ function afficheForm() {
       <BaseInput>
         <template v-slot:label>Date</template>
         <template v-slot:input>
-          <input
-            v-model="date"
-            class="input"
-            type="date"
-            placeholder="Entrez une date"
-          />
+          <input v-model="date" class="input" type="date" placeholder="Entrez une date" />
           <!--           <input
             type="date"
             data-display-mode="inline"
