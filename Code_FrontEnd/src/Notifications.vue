61f3a67aee52ae1401b8bc616a5d79cb6e2ec659
Stéphane Sordet <stsordet@gmail.com>
2022-06-15 11:23:37 +0200

    fixed bug

--- Code_FrontEnd/src/Notifications.vue
+++ Code_FrontEnd/src/Notifications.vue
@@ -70,7 +70,7 @@ const allNotifications = computed(() => {
     });
   }
 
-  return tabNotifications.();
+  return tabNotifications.reverse();
 });
 
 // ---------------------- Boolean for showing the modal form -----------------------------
